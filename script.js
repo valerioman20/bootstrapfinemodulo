9645c85a20157e837f95ac8ab85b95a45f51f2f5
valerioman20 <valerioman32@gmail.com>
2024-04-05 15:55:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//effetto di dissolvenza al passaggio del mouse sulle immagini
+
+document.querySelectorAll('.col img').forEach(image => {
+    image.addEventListener('mouseenter', () => {
+        image.style.transition = 'transform 0.5s';
+        image.style.transform = 'scale(1.1)';
+    });
+    
+    image.addEventListener('mouseleave', () => {
+        image.style.transition = 'transform 0.5s';
+        image.style.transform = 'scale(1)';
+    });
+});

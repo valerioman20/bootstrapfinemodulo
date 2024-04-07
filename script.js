b31f4b12188701eb84213caeb59ff990b40d9d99
valerioman20 <valerioman32@gmail.com>
2024-04-07 09:12:12 +0200

    Miglioramento commenti

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //effetto di dissolvenza al passaggio del mouse sulle immagini
 
+ // Con il ciclo forEach itero ogni immagine
 document.querySelectorAll('.col img').forEach(image => {
-    image.addEventListener('mouseenter', () => {
+    image.addEventListener('mouseenter', () => {    // Quando il mouse entra nell'immagine, viene eseguito il seguente codice
         image.style.transition = 'transform 0.5s';
         image.style.transform = 'scale(1.1)';
     });
     
-    image.addEventListener('mouseleave', () => {
+    image.addEventListener('mouseleave', () => { // Quando il mouse esce dalll'immagine, viene eseguito il seguente codice
         image.style.transition = 'transform 0.5s';
         image.style.transform = 'scale(1)';
     });
